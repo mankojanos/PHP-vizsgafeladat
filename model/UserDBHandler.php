9b34a843b8c7f47588b5f48c607b4f96cd3432a7
Janos <47101526+mankojanos@users.noreply.github.com>
2021-03-12 08:27:02 +0100

    Create UserDBHandler.php

---
+++ model/UserDBHandler.php
@@ -0,0 +1,50 @@
+<?php
+require_once __DIR__ . '/../global/PDOconnection.php';
+
+class UserDBHandler {
+    private PDO $db;
+
+    public function __construct() {
+        $this->db = PDOconnection::getCopy();
+    }
+
+    /**
+     * Save user to database
+     * @param User $user user for save
+     * @throws PDOException if database error
+     */
+    public function save(User $user) {
+        $query = $this->db->prepare('INSERT INTO users VALUES (?, ?)');
+        $query->execute(array($user->getUsername(), $user->getPassword()));
+    }
+
+    /**
+     * Check the username in database
+     * @param string $userName username for check
+     * @return bool true if username was found in database, false if it's not.
+     */
+    public function checkUsername (string $userName) {
+        $query = $this->db->prepare("SELECT COUNT(username) FROM users WHERE username = ?");
+        $query->execute(array($userName));
+        if($query->fetchColumn() > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * check the username and the password is the database
+     * @param string $username username for check
+     * @param string $password password for check
+     * @return bool true if the user is valid, false if invalid
+     */
+    public function userValidate(string $username, string $password) {
+        $query = $this->db->prepare("SELECT COUNT(username) FROM users WHERE username = ? AND password = ?");
+        $query->execute(array($username, $password));
+
+        if($query->fetchColumn() > 0) {
+            return true;
+        }
+        return false;
+    }
+}
