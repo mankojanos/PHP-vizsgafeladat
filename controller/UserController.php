c5400e15db987eb5a075599940fab10fce195011
Janos <47101526+mankojanos@users.noreply.github.com>
2021-04-11 14:11:35 +0200

    Update UserController.php

--- controller/UserController.php
+++ controller/UserController.php
@@ -26,7 +26,7 @@ class UserController extends MainController {
     public function login() {
         if(filter_input(INPUT_POST, 'username')) {
             $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
-            if($this->userDBHandler->userValidate($username, filter_input(INPUT_POST, 'passw', FILTER_SANITIZE_SPECIAL_CHARS))) {
+            if($this->userDBHandler->userValidate($username, filter_input(INPUT_POST, 'passwd', FILTER_SANITIZE_SPECIAL_CHARS))) {
                 $_SESSION['actual_user'] = $username;
                 $this->view->redirect('posts', 'index');
             } else {
@@ -38,5 +38,34 @@ class UserController extends MainController {
         $this->view->render('users', 'login');
     }
 
-    
+    /**
+     * Sign up user
+     * GET request  => render the page
+     * POST request => register user
+     */
+    public function signUp() {
+        $user = new User();
+
+        if(filter_input(INPUT_POST, 'username')) {
+            $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
+            $user->setUsername($username);
+            $user->setPassword(filter_input(INPUT_POST, 'passwd', FILTER_SANITIZE_SPECIAL_CHARS));
+            try {
+                $user->registrationValidation();
+                if(!$this->userDBHandler->checkUsername($username)) {
+                    $this->userDBHandler->save($user);
+                    $this->view->setMessageSession('The user: ' . $user->getUsername() . ' is registered. Please Log in.');
+                    $this->view->redirect('users', 'login');
+                } else {
+                    $errors = [];
+                    $errors['username'] = 'This username was registered before';
+                    $this->view->setVar('errors', $errors);
+                }
+            } catch (ValidationException $e) {
+                $this->view->setVar('errors', $e->getErrors());
+            }
+        }
+        $this->view->setVar('user', $user);
+        $this->view->render('users', 'signUp');
+    }
 }
