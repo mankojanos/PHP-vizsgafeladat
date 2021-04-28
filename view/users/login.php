71bce643765d18c0b3a8934ce76a67947b013190
Janos <47101526+mankojanos@users.noreply.github.com>
2021-04-28 09:43:23 +0200

    Create login.php

---
+++ view/users/login.php
@@ -0,0 +1,25 @@
+<?php
+require_once __DIR__ . '/../../global/ViewHandler.php';
+$view = ViewHandler::getCopy();
+$view->setVar('title', 'Login');
+$errors = $view->getErrors('errors');
+?>
+<h1>Login to page</h1>
+<?php print_r($errors); ?>
+<form method="post" action="index.php?controller=users&action=login">
+    <label for="username">Username:</label>
+    <input type="text" name="username">
+    <label for="passwd">Password:</label>
+    <input type="password" name="passwd">
+    <input type="submit" name="submit" value="Login">
+</form>
+
+<p>
+    <a href="index.php?controller=users&action=signUp">Sign up</a>
+</p>
+
+<?php
+$view->defaultViewLoad();
+?>
+
+
