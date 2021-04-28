2fb5e11465a788fd91e42bee70b296057bb9fd06
Janos <47101526+mankojanos@users.noreply.github.com>
2021-04-28 09:17:46 +0200

    Create main.php
    Create view

---
+++ view/layouts/main.php
@@ -0,0 +1,40 @@
+<?php
+require_once __DIR__ . '/../../global/ViewHandler.php';
+$view = ViewHandler::getCopy();
+$actualUser = $view->getVar('actualuser');
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Forum Page</h1>
+<ul>
+    <li><a href="index.php?controller=topics&action=index">Topics</a></li>
+    <?php
+    if(isset($actualUser)) {
+        echo '<li> Welcome ' . $actualUser .'. <a href="index.php?controller=users&action=logOut">logout</a></li>';
+    } else {
+        echo '<li><a href="index.php?controller=users&action=login">Login</a></li>';
+    }
+    ?>
+</ul>
+<div>
+    <?php
+        echo $view->getMessageSession();
+    ?>
+    <br>
+    <?php
+    echo $view->getView(ViewHandler::DEFAULT_VIEW);
+    ?>
+</div>
+<footer>Developer: MJ</footer>
+</body>
+</html>
+
+
