02deac97e334397d0b074b70cd3f33ea830601a0
Janos <47101526+mankojanos@users.noreply.github.com>
2021-04-28 09:43:05 +0200

    Update main.php

--- view/layouts/main.php
+++ view/layouts/main.php
@@ -1,5 +1,4 @@
 <?php
-require_once __DIR__ . '/../../global/ViewHandler.php';
 $view = ViewHandler::getCopy();
 $actualUser = $view->getVar('actualuser');
 ?>
@@ -10,7 +9,7 @@ $actualUser = $view->getVar('actualuser');
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title><?php $view->getVar('title', 'MVC forum') ?></title>
 </head>
 <body>
 <h1>Forum Page</h1>
