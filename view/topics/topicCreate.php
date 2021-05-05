fe0e37ad5f8a2527033a19a424193d6f3a755626
Janos <47101526+mankojanos@users.noreply.github.com>
2021-05-05 15:50:01 +0200

    Create topicCreate.php

---
+++ view/topics/topicCreate.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . '/../../global/ViewHandler.php';
+$view = ViewHandler::getCopy();
+$topic = $view->getVar('topic');
+$errors = $view->getVar('errors');
+$view->setVar('title', 'topic create');
+
+var_dump($errors);
+?>
+<h1>Topic create</h1>
+<form method="post" action="index.php?controller=topics&actiion=topicCreate">
+    <label>Title</label>
+    <input type="text" name="title" value="<?php echo $topic->getTitle(); ?>">
+    <label>Content</label>
+    <textarea name="content">
+        <?php echo $topic->getContent(); ?>
+    </textarea>
+    <input type="submit" name="submit" value="OK">
+</form>
